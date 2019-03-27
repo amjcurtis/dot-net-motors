573c87cc0ce81b80d57f3967d562d62b12e6a5a3
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-27 16:28:39 -0700

    Add project files.

---
+++ dot-net-motors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dot_net_motors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
