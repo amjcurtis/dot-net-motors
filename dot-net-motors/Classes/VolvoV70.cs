c0ac55e0b530fb5cc93314745d8e79d92282bc14
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-27 19:59:34 -0700

    Add class for Volvo

--- dot-net-motors/Classes/VolvoV70.cs
+++ dot-net-motors/Classes/VolvoV70.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace dot_net_motors.Classes
 {
-    class VolvoV70
+    class VolvoV70 : StationWagon
     {
+        // Overridden properties
+        public override decimal Price { get; set; } = 38000m;
+        public override bool SunRoof { get; set; } = true;
+
+        // Overridden methods
+        public override void Drive()
+        {
+            Console.WriteLine("Volvo can drive!");
+        }
+
+        public override string PlayRadio()
+        {
+            return "Volvo plays tunes!";
+        }
     }
 }
